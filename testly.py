e449e6e39bcd00f7aa09b0c7a98e447cc385a706
Pratiksha <agap@oregonstate.edu>
2021-03-07 19:50:57 -0800

    Wrote second test, fixed the problem, successful

--- testly.py
+++ testly.py
@@ -5,8 +5,10 @@ import checkLeapYear
 
 class TestCase(unittest.TestCase):
 	def test1(self):
-		self.assertEqual(checkLeapYear.ly(2020), "Not a Leap year");
+		self.assertEqual(checkLeapYear.ly(2020), "Leap year");
 
+	def test2(self):
+		self.assertEqual(checkLeapYear.ly(2021), "Not a leap year");
 
 if __name__ == '__main__':
 	unittest.main();
